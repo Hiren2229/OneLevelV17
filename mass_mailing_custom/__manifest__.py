a6d95b5c375a10710a3d3ff8776a33ca23ea870d
pratikdevaliya <97970312+pratikdevaliya@users.noreply.github.com>
2024-04-13 15:12:45 +0530

    Added mass_mailing_custom V17

---
+++ mass_mailing_custom/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#                                                                            #
+# Part of Aardug. (Website: www.aardug.nl).                                  #
+# See LICENSE file for full copyright and licensing details.                 #
+#                                                                            #
+##############################################################################
+
+{
+	'name': 'Mass Mailing',
+	'version': '17.0.1.0',
+	'license': 'OPL-1',
+	'summary': 'Mass Mailing',
+	'category': '',
+	'description':
+		"""
+		Features:
+			This module allows to Show Mail Server even when the Developer Mode is Off.
+		""",
+	'author': 'Aardug, Arjan Rosman',
+	'website': 'http://www.aardug.nl/',
+	'support': 'arosman@aardug.nl',
+	'depends': [
+		'mass_mailing',
+	],
+	'data': [
+		'views/mass_mailing.xml',
+	],
+}
